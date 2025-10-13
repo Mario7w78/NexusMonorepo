f3e98eca1627e0c53558a8c2e89034d4214b01f5
Mario7w78 <mariox0603@gmail.com>
2025-10-12 19:36:46 -0500

    update

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>NEXUS - Repositorio de Ideas</Text>
+      <Text>NEXUS</Text>
       <StatusBar style="auto" />
     </View>
   );
